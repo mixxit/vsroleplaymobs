228b25761470cf99370c3d109c53cb6fdf115788
Michael Bolland <mixxit@users.noreply.github.com>
2022-03-31 12:17:49 +0100

    empty project

---
+++ mods/vsroleplaymobs/src/VSRoleplayMobsMod.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Drawing;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Datastructures;
+using Vintagestory.API.MathTools;
+using Vintagestory.API.Server;
+
+namespace vsroleplaymobds.src
+{
+    public class VSRoleplayMobsMod : ModSystem
+    {
+        Random rand;
+        private ICoreServerAPI csapi;
+
+        public override void StartPre(ICoreAPI api)
+        {
+            base.StartPre(api);
+        }
+
+        public override bool ShouldLoad(EnumAppSide side)
+        {
+            return true;
+        }
+
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            this.csapi = api;
+            base.StartServerSide(api);
+        }
+
+        public override void Start(ICoreAPI api)
+        {
+            rand = new Random();
+            base.Start(api);
+
+        }
+
+        public override double ExecuteOrder()
+        {
+            /// Worldgen:
+            /// - GenTerra: 0 
+            /// - RockStrata: 0.1
+            /// - Deposits: 0.2
+            /// - Caves: 0.3
+            /// - Blocklayers: 0.4
+            /// Asset Loading
+            /// - Json Overrides loader: 0.05
+            /// - Load hardcoded mantle block: 0.1
+            /// - Block and Item Loader: 0.2
+            /// - Recipes (Smithing, Knapping, Clayforming, Grid recipes, Alloys) Loader: 1
+            /// 
+            return 1.1;
+        }
+    }
+}
